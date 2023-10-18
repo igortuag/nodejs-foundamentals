a7f4ad8c4e5094ee0a422df9d9974cc097296558
Tuag <igortuag@gmail.com>
2023-10-18 08:33:14 -0300

    add OneToHundredStream

--- streams/foundamentals.js
+++ streams/foundamentals.js
@@ -12,4 +12,22 @@
 
 // with stream its possible to start processing the data while its being uploaded
 
-process.stdin.pipe(process.stdout);
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    if (i > 100) {
+      this.push(null);
+    } else {
+      this.push(i);
+    }
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
