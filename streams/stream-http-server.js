164212adebe9161ac56e097b6c4b1ead9c81cab1
Tuag <igortuag@gmail.com>
2023-10-20 08:50:43 -0300

    add inverse on request

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,5 +1,20 @@
 import http from "node:http";
+import { Transform } from "node:stream";
 
-const server = http.createServer((req, res) => {});
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = parseInt(chunk.toString()) * -1;
+
+    console.log(transformed);
+
+    callback(null, Buffer.from(transformed.toString()));
+  }
+}
+
+const server = http.createServer((req, res) => {
+  return req
+    .pipe(new InverseNumberStream())
+    .pipe(res);
+});
 
 server.listen(3334);
