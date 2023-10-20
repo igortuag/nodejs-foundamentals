6190f8868f3b55bec28f087e87d4f9845af14e2b
Tuag <igortuag@gmail.com>
2023-10-20 08:48:40 -0300

    add fake upload api

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,17 @@
+import { Readable } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        this.push(`${i}\n`);
+      }
+    }, 1000);
+  }
+}
