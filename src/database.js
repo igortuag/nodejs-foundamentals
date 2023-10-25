c2aa9fa6757951bbbcbcaf5907d8579a306515f1
Tuag <igortuag@gmail.com>
2023-10-25 08:58:33 -0300

    add actions to database class

--- src/database.js
+++ src/database.js
@@ -1,3 +1,17 @@
 export class Database {
-  database = {}
+  database = {};
+
+  select(table) {
+    const data = this.database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.database[table])) {
+      this.database[table].push(data);
+    } else {
+      this.database[table] = [data];
+    }
+  }
 }
