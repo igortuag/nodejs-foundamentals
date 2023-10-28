1af2dd18a1384a8de38cb6568b534842b745aa6f
Tuag <igortuag@gmail.com>
2023-10-28 05:48:45 -0300

    add route to create users

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,32 @@
+import { Database } from "./database";
+
 export const routes = [
   {
     method: "GET",
     path: "/users",
     handle: async (req, res) => {
-      const users = await req.database.select("users");
+      const users = await Database.select("users");
 
       return res
         .setHeader("Content-Type", "application/json")
         .end(JSON.stringify(users));
+    }
+  },
+  {
+    method: "POST",
+    path: "/users",
+    handle: async (req, res) => {
+      const { name, email } = req.body;
+
+      const user = {
+        id: randomUUID(),
+        name,
+        email
+      };
+
+      await Database.insert("users", user);
+
+      return res.writeHead(201).end();
     }
   }
 ];
