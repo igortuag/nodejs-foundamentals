c020d440692bcabcc90f7a41ce44e744d5ef186f
Tuag <igortuag@gmail.com>
2023-10-31 09:10:36 -0300

    add pathRegex

--- src/util/build-route-path.js
+++ src/util/build-route-path.js
@@ -4,6 +4,8 @@ export function buildRoutePath(path) {
 
   const pathWithParams = path.replaceAll(routeParamsRegex, '([a-zA-Z0-9\-\_]+)');
 
+  const pathRegex = new RegExp(`^${pathWithParams}$`);
+
   return path.replace(routeParamsRegex, (_, paramName) => {
     return `(?<${paramName}>[a-zA-Z0-9-]*)`;
   });
