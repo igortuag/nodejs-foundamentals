a9bb74f4e3178dd32fda7f0e7c1ef54b1a4a5761
Tuag <igortuag@gmail.com>
2023-10-31 09:19:10 -0300

    adjust name

--- src/util/build-route-path.js
+++ src/util/build-route-path.js
@@ -2,7 +2,7 @@
 export function buildRoutePath(path) {
   const routeParamsRegex = /:([a-zA-Z]*)/g;
 
-  const pathWithParams = path.replaceAll(routeParamsRegex, "(?<id>[a-zA-Z0-9-_]+)");
+  const pathWithParams = path.replaceAll(routeParamsRegex, "(?<$1>[a-zA-Z0-9-_]+)");
 
   const pathRegex = new RegExp(`^${pathWithParams}$`);
 
