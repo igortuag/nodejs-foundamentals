089497a3866eb8a7239d72a5f101100f84c67402
Tuag <igortuag@gmail.com>
2023-10-31 09:15:48 -0300

    return path regex

--- src/util/build-route-path.js
+++ src/util/build-route-path.js
@@ -2,11 +2,9 @@
 export function buildRoutePath(path) {
   const routeParamsRegex = /:([a-zA-Z]*)/g;
 
-  const pathWithParams = path.replaceAll(routeParamsRegex, '([a-zA-Z0-9\-\_]+)');
+  const pathWithParams = path.replaceAll(routeParamsRegex, "([a-zA-Z0-9-_]+)");
 
   const pathRegex = new RegExp(`^${pathWithParams}$`);
 
-  return path.replace(routeParamsRegex, (_, paramName) => {
-    return `(?<${paramName}>[a-zA-Z0-9-]*)`;
-  });
+  return pathRegex;
 }
